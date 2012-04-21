b2a4a86246c92665b01eaaf4c5a643ed77a05356
Aaron <schnarf@gmail.com>
2012-04-20 21:40:07 -0400

    Every implemented operator is tested.

--- tests/operators.cpp
+++ tests/operators.cpp
@@ -38,3 +38,84 @@ TEST_F(Operators, Add) {
       << ", input=" << inputs[i];
   }
 }
+
+TEST_F(Operators, Sub) {
+  apply(inputs.size(), &inputs[0], &actual[0], _x - 4.0f);
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    expected[i] = inputs[i] - 4.0f;
+  }
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    EXPECT_FLOAT_EQ(expected[i], actual[i])
+      << "index=" << i
+      << ", input=" << inputs[i];
+  }
+}
+
+TEST_F(Operators, Mul) {
+  apply(inputs.size(), &inputs[0], &actual[0], _x * 4.0f);
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    expected[i] = inputs[i] * 4.0f;
+  }
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    EXPECT_FLOAT_EQ(expected[i], actual[i])
+      << "index=" << i
+      << ", input=" << inputs[i];
+  }
+}
+
+TEST_F(Operators, Max) {
+  apply(inputs.size(), &inputs[0], &actual[0], max(_x, 4.0f));
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    expected[i] = std::max(inputs[i], 4.0f);
+  }
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    EXPECT_FLOAT_EQ(expected[i], actual[i])
+      << "index=" << i
+      << ", input=" << inputs[i];
+  }
+}
+
+TEST_F(Operators, Min) {
+  apply(inputs.size(), &inputs[0], &actual[0], min(_x, 4.0f));
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    expected[i] = std::min(inputs[i], 4.0f);
+  }
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    EXPECT_FLOAT_EQ(expected[i], actual[i])
+      << "index=" << i
+      << ", input=" << inputs[i];
+  }
+}
+
+TEST_F(Operators, Abs) {
+  apply(inputs.size(), &inputs[0], &actual[0], abs(_x));
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    expected[i] = std::abs(inputs[i]);
+  }
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    EXPECT_FLOAT_EQ(expected[i], actual[i])
+      << "index=" << i
+      << ", input=" << inputs[i];
+  }
+}
+
+TEST_F(Operators, Sqrt) {
+  apply(inputs.size(), &inputs[0], &actual[0], sqrt(_x));
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    expected[i] = std::sqrt(inputs[i]);
+  }
+  for (unsigned i=0; i<inputs.size(); ++i) {
+    // Negative numbers should be NaN
+    if (inputs[i] < 0) {
+      EXPECT_TRUE(actual[i] != actual[i])
+        << "Expected NaN. "
+        << "index=" << i
+        << ", input=" << inputs[i];
+      continue;
+    }
+
+    EXPECT_FLOAT_EQ(expected[i], actual[i])
+      << "index=" << i
+      << ", input=" << inputs[i];
+  }
+}
